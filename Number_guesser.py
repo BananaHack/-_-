2ccdd17ca211abe8fcab875d4480ba4a79b32a15
BananaHack <71518140+BananaHack@users.noreply.github.com>
2021-10-09 18:48:11 +0300

    Update Number_guesser.py

--- Number_guesser.py
+++ Number_guesser.py
@@ -1,25 +1,16 @@
 from random import *
-
 num, flag = randint(1, 100), False
-
 while flag == False:
-
     the_input = int(input('Угадайте загаданное число:  '))
-
     if the_input < num:
-
         print('Слишком мало, попробуйте еще раз\n')
-
         continue
 
     elif the_input > num:
-
         print('Слишком много, попробуйте еще раз\n')
-
         continue
 
     elif the_input == num:
-
         print('Вы угадали, поздравляем!')
         repeat = input('\nВведите "Ещё", чтобы сыграть ещё раз или "Выход", чтобы закрыть игру:  ')
         while repeat != 'Ещё' or repeat != 'Выход':
@@ -28,6 +19,5 @@ while flag == False:
                 print()
                 break
             elif repeat == 'Выход':
-                #flag = True
+                flag = True
                 exit()
-
